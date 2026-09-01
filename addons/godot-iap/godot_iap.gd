443767aa878c26d949aee6c808aa7dbe01df6c6b
Think13 <denna.hirlan.74@gmail.com>
2026-09-01 15:14:49 +0700

    fix: debug IAP

--- addons/godot-iap/godot_iap.gd
+++ addons/godot-iap/godot_iap.gd
@@ -373,7 +373,7 @@ func _fetch_products_raw(request: Dictionary) -> Dictionary:
 			last_products_error = String(signal_result.get("error", ""))
 			if signal_result.get("products") is Array:
 				products_array = signal_result.get("products")
-			elif signal_result.get("success", false):
+			else:
 				var products_json = signal_result.get("productsJson", "[]")
 				var parsed = JSON.parse_string(products_json)
 				if parsed is Array:
